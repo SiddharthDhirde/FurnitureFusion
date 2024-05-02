1d1e6d05909960169a5948a31dd97fc5ee9b74ad
Siddharth Dhirde <siddharthdhirde09@gmail.com>
2024-05-02 13:58:20 +0530

    change name to Furniture Fusion

--- src/views/auth/forgot_password/index.jsx
+++ src/views/auth/forgot_password/index.jsx
@@ -16,7 +16,7 @@ const ForgotPassword = () => {
   const [field, setField] = useState({});
 
   useScrollTop();
-  useDocumentTitle('Forgot Password | Salinaka');
+  useDocumentTitle('Forgot Password | Furniture Fusion');
   useEffect(() => {
     if (didMount) {
       setForgotPWStatus(authStatus);
